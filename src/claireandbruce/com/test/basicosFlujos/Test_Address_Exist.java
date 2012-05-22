d31227ef757dbcc2205327da79c4321ae608d2ba
clandbruceqa <newshorecb+qa@gmail.com>
2012-05-22 11:52:07 -0500

    ok

--- src/claireandbruce/com/test/basicosFlujos/Test_Address_Exist.java
+++ src/claireandbruce/com/test/basicosFlujos/Test_Address_Exist.java
@@ -1,5 +1,42 @@
 package claireandbruce.com.test.basicosFlujos;
 
-public class Test_Address_Exist {
+import lib.Claireandbruce;
+import lib.Helper;
+import basics.ClaireandbruceTestCase;
+/* 
+ * Esta clase verifica que exista al menos una dirección guardada por defecto, 
+ * 
+ * 
+ * 
+ * 
+ * */
+public class Test_Address_Exist extends ClaireandbruceTestCase {
+	
+	public void CBT_Addres() throws Exception{
+		
+	
+	selenium.open("");
+	selenium.waitForPageToLoad("15000");
+	int i =(int)(Math.random()*100);
+	//El usuario no debe tener su sesión iniciada
+	if(!selenium.isElementPresent("//a[contains(text(), 'Salir')]")){
+		Claireandbruce.login(selenium, username, password);
+	}
+	
+	
+	Helper.log("Ambiente de prueba: "+selenium.getLocation());
+
+	
+	//Ingresar a traves del botón 'TUS DIRECCIONES' Área privada'
+	if(selenium.isElementPresent("xpath=html/body/div/div[2]/div[1]/div/div/div[2]/a[2]/div/div/p")){
+		selenium.click("xpath=html/body/div/div[2]/div[1]/div/div/div[2]/a[2]/div/div/p");
+	} else {//Ingresar a través del link 'Modificar o añadir dirección'
+		selenium.click("xpath=html/body/div/div[2]/div[2]/div[1]/div[2]/ul/div[3]/li/a/span");
+	}
+	
+	selenium.waitForPageToLoad("30000");
+	
+	}
+	
 
 }
