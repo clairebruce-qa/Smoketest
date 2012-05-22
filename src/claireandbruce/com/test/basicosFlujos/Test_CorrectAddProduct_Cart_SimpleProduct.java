7b4af63f68bae7539d8ee5114431b016d16111d6
claireandbruceqa <newshorecb+qa@gmail.com>
2012-05-22 09:03:02 -0500

    Basico Flujos

---
+++ src/claireandbruce/com/test/basicosFlujos/Test_CorrectAddProduct_Cart_SimpleProduct.java
@@ -0,0 +1,33 @@
+package claireandbruce.com.test.basicosFlujos;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import basics.ClaireandbruceTestCase;
+
+/**
+ * Se automatiza el añadir un producto simple a la cesta.
+ * @author NEWSHORE
+ *
+ */
+public class Test_CorrectAddProduct_Cart_SimpleProduct extends ClaireandbruceTestCase {
+
+	@Test
+	public void CBT_SimpleProduct() {
+		
+		//Para efecto de la prueba
+		selenium.open("");
+		selenium.waitForPageToLoad("15000");
+		
+		//Se selecciona una categoría entre Bolsos y Accesorios (Categorías que poseen productos simples)
+		int categoria = (int) (Math.random()*(4-3) +3);
+		
+		if(selenium.isElementPresent("xpath=.//*[@id='nav']/li["+categoria+"]/h2/a/span/cufon/canvas")){
+			selenium.click("xpath=.//*[@id='nav']/li["+categoria+"]/h2/a/span/cufon/canvas");
+		} else {
+			Assert.fail("ERROR. Categoría no encontrada");
+		}
+	}
+	
+}
---
+++ src/claireandbruce/com/test/basicosFlujos/Test_Pay_Credit_Card.java
@@ -0,0 +1,8 @@
+package claireandbruce.com.test.basicosFlujos;
+
+public class Test_Pay_Credit_Card {
+
+	
+	
+	
+}
