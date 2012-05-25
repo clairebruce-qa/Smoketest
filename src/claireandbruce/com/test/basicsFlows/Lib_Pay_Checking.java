e20b5e2268dd60ca705060a3e5784f9a6f604eef
claireandbruceqa <newshorecb+qa@gmail.com>
2012-05-25 08:57:29 -0500

    Dirección de factura diferente a la de envío

---
+++ src/claireandbruce/com/test/basicsFlows/Lib_Pay_Checking.java
@@ -0,0 +1,27 @@
+package claireandbruce.com.test.basicsFlows;
+
+import lib.Helper;
+
+import com.thoughtworks.selenium.Selenium;
+import static org.junit.Assert.assertEquals;
+import basics.ClaireandbruceTestCase;
+
+public class Lib_Pay_Checking extends ClaireandbruceTestCase {
+
+	public static void pay_checking(Selenium selenium) {
+
+		Helper.log("\nPAGO CON CHEQUE");
+		if(selenium.getTitle().equals("Compra ahora")){
+			//Se selecciona método de pago: Cheque
+			selenium.click("id=p_method_checkmo");
+			//Se aceptan términos y condiciones
+			selenium.click("id=agreement-1");
+			//Se presiona el botón PAGAR
+			selenium.click("xpath=//form/div/button");
+			selenium.waitForPageToLoad("30000");
+			assertEquals(selenium.getText("xpath=//p"),"Recibirás un e-mail con tu confirmación de compra a:");
+		} else {
+			Helper.log("No se encuentra actualmente en la interfaz 'Compra ahora'");
+		}
+	}
+}
---
+++ src/claireandbruce/com/test/basicsFlows/Lib_With_Purchase_and_Other_Address.java
@@ -0,0 +1,47 @@
+package claireandbruce.com.test.basicsFlows;
+
+import lib.Helper;
+
+import com.thoughtworks.selenium.Selenium;
+import basics.ClaireandbruceTestCase;
+
+public class Lib_With_Purchase_and_Other_Address extends ClaireandbruceTestCase {
+	
+	public static void with_Purchase_and_Other_Address (Selenium selenium){
+		
+		//Para efectos de prueba unitaria
+		selenium.open(ClaireandbruceUrl+"/es_es/aitcheckout/checkout/index/");
+		selenium.waitForPageToLoad("15000");
+		//Se verifica que se encuentre seleccionado "Necesito una factura" 
+		if(selenium.getValue("id=showFakeBilling").equals("off")){
+			selenium.click("id=showFakeBilling");
+		}
+		
+		//Se verifica que se encuentre desactivado la selección de la dirección de factura es diferente a la de envío
+		if(selenium.getValue("id=showFakeBillingExtended").equals("on")){
+			Helper.log("Se muestra activo sin seleccionarlo el usuario la opción 'La dirección de factura es diferente a la de envío'");
+			selenium.click("id=showFakeBillingExtended");		 //Se desactiva para que al seleccionarlo de nuevo muestre los campos necesarios	
+		}
+		//Se selecciona la opción 'La dirección de factura es diferente a la de envío'
+		selenium.click("id=showFakeBillingExtended");
+		
+		//Se verifican que los campos 'Nombre' y 'Apellidos' se encuentren diligenciados
+		if(selenium.getValue("id=billing:firstname").equals("")){
+			Helper.log("EL CAMPO NOMBRE NO SE ENCUENTRA DILIGENCIADO CON LA INFORMACIÓN DEL USUARIO");
+			selenium.type("id=billing:firstname","Claire");
+		}
+		if(!selenium.getValue("id=billing:lastname").equals("")){
+			Helper.log("EL CAMPO APELLIDOS NO SE ENCUENTRA DILIGENCIADO CON LA INFORMACIÓN DEL USUARIO");
+			selenium.type("id=billing:firstname","QA");
+		}
+		//Se ingresan los datos (Dirección1. Población, Código Postal, Telefono)
+		selenium.type("id=billing:street1", "AV 5 34 6");
+		selenium.type("id=billing:city", "Barcelona");
+		selenium.click("xpath=//li[5]/div[2]/div/div[2]/div/div/div[23]");
+		selenium.type("id=billing:postcode", "50080");
+		selenium.type("id=billing:telephone", "920512354");
+		
+		
+		
+	}
+}
