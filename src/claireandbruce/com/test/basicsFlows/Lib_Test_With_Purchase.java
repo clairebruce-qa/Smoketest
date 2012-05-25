aea24d408acfa31c2db502013286d8a346ff74f9
clandbruceqa <newshorecb+qa@gmail.com>
2012-05-25 06:40:29 -0500

    se agregan clases nuevas

---
+++ src/claireandbruce/com/test/basicsFlows/Lib_Test_With_Purchase.java
@@ -0,0 +1,25 @@
+package claireandbruce.com.test.basicsFlows;
+
+import com.thoughtworks.selenium.Selenium;
+
+import basics.ClaireandbruceTestCase;
+
+
+
+
+
+public class Lib_Test_With_Purchase  extends ClaireandbruceTestCase{
+	
+	
+	
+	public static void shippingWithPurchase(Selenium selenium){
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
