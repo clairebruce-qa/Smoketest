9e8f781405d8738089ba42341a6f381fa44cecf8
claireandbruceqa <newshorecb+qa@gmail.com>
2012-05-29 12:17:53 -0500

    Add Suite

--- src/claireandbruce/com/test/navigationWeb/CBT31_Test_Home_Page_Navigability_1_Navigability_for_Look_Not_Login.java
+++ src/claireandbruce/com/test/navigationWeb/CBT31_Test_Home_Page_Navigability_1_Navigability_for_Look_Not_Login.java
@@ -12,71 +12,21 @@ public class CBT31_Test_Home_Page_Navigability_1_Navigability_for_Look_Not_Login
 	@Test
 	public void CBT31 () throws Exception{
 		
+		//Pruebas unitarias
 		selenium.open("");
 		selenium.waitForPageToLoad("20000");
-		//Se ingresa al primer total look
-		if(selenium.isElementPresent("xpath=//div/a/img")){
-			//Se ingresa al look y se compara con el texto Compra el total look ubicado en la pagina
-			selenium.click("xpath=//div/a/img");
+		int look= 2;
+		while(selenium.isElementPresent("xpath=//div["+look+"]/a/img")){
+			selenium.click("xpath=//div["+look+"]/a/img");
 			selenium.waitForPageToLoad("15000");
-			if(selenium.isElementPresent("xpath=//*[@alt='/ES/']")){
-				Helper.log("PAGINA NO DISPONIBLE");
-				selenium.click("xpath=//div[3]/a/cufon/canvas");
-			}else {				
-				assertTrue(selenium.getText("id=total-look").equals("Compra el total look"));
-				selenium.click("link=Inicio");
-			}			
-			selenium.waitForPageToLoad("10000");
-		}
-		
-		//Se ingresa al segundo total look
-		if(selenium.isElementPresent("xpath=//div[2]/a/img")){
-			//Se ingresa al look y se compara con el texto Compra el total look ubicado en la pagina
-			selenium.click("xpath=//div[2]/a/img");
-			selenium.waitForPageToLoad("15000");
-			if(selenium.isElementPresent("xpath=//*[@alt='/ES/']")){
-				Helper.log("PAGINA NO DISPONIBLE");
-				selenium.click("xpath=//div[3]/a/cufon/canvas");
-			}else {				
-				assertTrue(selenium.getText("id=total-look").equals("Compra el total look"));
-				selenium.click("link=Inicio");
+			String migaDePan = selenium.getText("css=span.breadcrumbs-present");
+			String nombreLook = selenium.getTitle();
+			if(migaDePan.equals(nombreLook)) {
+				Helper.log("Look "+nombreLook+" presente");
+			} else {
+				Helper.log("El Look "+nombreLook+" no se encuentra presente");
 			}
-			selenium.waitForPageToLoad("10000");
-		}
-		
-		//Se ingresa al tercer total look
-		if(selenium.isElementPresent("xpath=//div[3]/a/div/div")){
-			//Se ingresa al look y se compara con el texto Compra el total look ubicado en la pagina
-			selenium.click("xpath=//div[3]/a/div/div");
-			selenium.waitForPageToLoad("15000");
-			if(selenium.isElementPresent("xpath=//div[3]/a/div/div")){
-				Helper.log("PAGINA NO DISPONIBLE");
-				selenium.click("xpath=//div[3]/a/cufon/canvas");
-			}else {				
-				assertTrue(selenium.getText("id=total-look").equals("Compra el total look"));
-				selenium.click("link=Inicio");
-			}			
-			selenium.waitForPageToLoad("10000");
-		}
-		
-		//Se ingresa al cuarto total look
-		if(selenium.isElementPresent("xpath=//div[4]/a/div")){
-			//Se ingresa al look y se compara con el texto Compra el total look ubicado en la pagina
-			selenium.click("xpath=//div[4]/a/div");
-			selenium.waitForPageToLoad("15000");
-			if(selenium.isElementPresent("xpath=//div[4]/a/div")){
-				Helper.log("PAGINA NO DISPONIBLE");
-				selenium.click("xpath=//div[3]/a/cufon/canvas");
-			}else {				
-				assertTrue(selenium.getText("id=total-look").equals("Compra el total look"));
-				selenium.click("link=Inicio");
-			}			
-			selenium.waitForPageToLoad("10000");
-		}
-		
-		if(!selenium.isElementPresent("xpath=//div/a/img") && !selenium.isElementPresent("xpath=//div[2]/a/img") &&
-			!selenium.isElementPresent("xpath=//div[3]/a/div/div") && !selenium.isElementPresent("xpath=//div[4]/a/div")){
-			Helper.log("NO SE ENCUENTRA NINGUN ENLACE A TOTAL LOOK POR IMAGENES EN HOMEPAGE");
-		}
+			look++;
+		}	
 	}
 }
---
+++ src/suites/Flows_Suite.java
@@ -0,0 +1,47 @@
+package suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import claireandbruce.com.test.basicsFlows.Lib_CorrectLogon_ValidUser_Accout;
+import claireandbruce.com.test.basicsFlows.Lib_Address_Exist;
+import claireandbruce.com.test.basicsFlows.Lib_ChangeUnits_OneProduct;
+import claireandbruce.com.test.basicsFlows.Lib_CorrectAddProduct_Cart_SimpleProduct;
+
+import claireandbruce.com.test.basicsFlows.Lib_PayPal;
+import claireandbruce.com.test.basicsFlows.Lib_TypeShipping;
+import claireandbruce.com.test.basicsFlows.Lib_Without_Purchase;
+import claireandbruce.com.test.flows.Test_Flow_I;
+import claireandbruce.com.test.flows.Test_Flow_II;
+import claireandbruce.com.test.flows.Test_Flow_III;
+import claireandbruce.com.test.flows.Test_Flow_IV;
+
+/**
+ * Flujo I login +  producto simple + con direccion + sin factura+ Pagado con Paypal
+ * @author YULIANA MARIA SAAVEDRA RUSSI
+ *
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	
+	
+	
+	
+	
+	Test_Flow_I.class,
+	
+	Test_Flow_II.class,
+	
+	Test_Flow_III.class,
+	
+	Test_Flow_IV.class
+	
+	
+})
+
+public class Flows_Suite {
+	
+
+
+}
