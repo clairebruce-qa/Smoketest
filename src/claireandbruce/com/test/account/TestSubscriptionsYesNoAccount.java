85f40deb081663e727327669edae1f519c4e5acf
claireandbruceqa <newshorecb+qa@gmail.com>
2012-07-04 16:10:48 -0500

    Suscripciones

--- src/claireandbruce/com/test/account/TestSubscriptionsYesNoAccount.java
+++ src/claireandbruce/com/test/account/TestSubscriptionsYesNoAccount.java
@@ -3,13 +3,8 @@ package claireandbruce.com.test.account;
 import static org.junit.Assert.*;
 import lib.Claireandbruce;
 import lib.Helper;
-
 import claireandbruce.com.test.basicsFlows.LibCorrectLogonValidUserAccout;
-
-import com.thoughtworks.selenium.*;
-
 import org.junit.Test;
-
 import basics.ClaireandbruceTestCase;
 
 /**
@@ -33,23 +28,23 @@ public class TestSubscriptionsYesNoAccount extends ClaireandbruceTestCase{
 	
 		if(!selenium.getTitle().equals("Mi cuenta")){
 			selenium.waitForPageToLoad("5000");
+			Helper.log("Se ingresa a link 'Mi cuenta'");
 			selenium.click("link=Mi cuenta");
 			selenium.waitForPageToLoad("15000");
 		}
 		
 		//--------verifica que el link de TUS SUSCRIPCIONES ESTE HABILITADO------------
-		//assertTrue(selenium.isElementPresent("//div[2]/li[2]/a/span"));
+		Helper.log("Se ingresa al link 'TUS SUSCRIPCIONES'");
 		selenium.click("xpath=//div[2]/li[2]/a/span");
 		selenium.waitForPageToLoad("30000");
-		
 		
 		if(!selenium.isTextPresent("Tus suscripciones")){
 			Helper.log("EL TEXTO ENCONTRADO ES EL SIGUIENTE"+selenium.getTitle());
 		}
 		
 		//--------------Verifica que los botones esten habilitados-----------------
-		assertTrue(selenium.isElementPresent("//input[@name='is_subscribed']")); //Si esta Suscrito a las noticias de C+B
-		assertTrue(selenium.isElementPresent("//input[@name='activate']"));//Si desearia recibir suscripciones de otras empresas asociadas
+		assertTrue("Suscripción a noticias c+b",selenium.isElementPresent("//input[@name='is_subscribed']")); //Si esta Suscrito a las noticias de C+B
+		assertTrue("Suscripción a otras empresas",selenium.isElementPresent("//input[@name='activate']"));//Si desearia recibir suscripciones de otras empresas asociadas
 		
 		/*
 		 * 1. Click on [Si, quiero recibirlas] [Alertas promocionales]
@@ -57,13 +52,16 @@ public class TestSubscriptionsYesNoAccount extends ClaireandbruceTestCase{
 		 * */
 		if(!((("on").equals(selenium.getValue("name=is_subscribed"))) && (("on").equals(selenium.getValue("xpath=(//input[@name='activate'])[2]")))))
 		{
+			Helper.log("Se realiza la suscripción a las alertas promocionales");
 			selenium.click("xpath=//input[@name='is_subscribed']");//Esta suscrito
+			Helper.log("Se elimina la suscripción a los emails de empresas y marcas");
 			selenium.click("xpath=(//input[@name='activate'])[2]");//No quiere suscribirse a los emails
 			
 			//-----------***************GUARDAR LOS CAMBIOS**************-------------------
+			Helper.log("Se hace clic en el botón 'GUARDAR'");
 			selenium.click("//button[@type='submit']");
 			selenium.waitForPageToLoad("30000");
-			System.out.println(""+selenium.isTextPresent("La suscripción ha sido guardada."));
+			Helper.log("Se verifica el mensaje de confirmación de la suscripción");
 			assertTrue(selenium.isTextPresent("La suscripción ha sido guardada."));
 		}
 		else
@@ -72,12 +70,15 @@ public class TestSubscriptionsYesNoAccount extends ClaireandbruceTestCase{
 			/*Se observa el mismo comportamiento al realizar la prueba con: 
 			 *No quiero recibirlas {on} [ALERTAS PROMOCIONALES]
 			 *Si, quiero {o} [E-mails de empresas y marcas que colaboran con C+B ]*/
-			
+			Helper.log("Se elimina la suscripción a alertas promocionales");
 			selenium.click("xpath=(//input[@name='is_subscribed'])[2]");//No esta suscrito
+			Helper.log("Se realiza la suscripción a otras empresas y marcas");
 			selenium.click("xpath=//input[@name='activate']");//Esta Suscrito a los emails
+			Helper.log("Se hace clic en el botón 'GUARDAR'");
 			selenium.click("//button[@type='submit']");
 			selenium.waitForPageToLoad("30000");
-			assertTrue(selenium.isTextPresent("La suscripción ha sido eliminada."));
+			Helper.log("Se verifica el mensaje de confirmación de la suscripción");
+			assertTrue("Mensaje no visualizado",selenium.isTextPresent("La suscripción ha sido eliminada."));
 		}
 	
 		Claireandbruce.logout(selenium);
